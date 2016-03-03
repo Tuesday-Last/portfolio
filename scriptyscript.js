5291e3597550568c8b63f7a0a8d924857405af17
Tuesday-Last <sgnortz@gmail>
2016-03-03 15:58:52 -0800

    got scafolding down

--- scriptyscript.js
+++ scriptyscript.js
@@ -1,2 +1,9 @@
 var projects = [];
 
+function Projects (project) {
+  this.title = project.title;
+  this.image = project.image;
+  this.blerb = project.blerb;
+  this.address = project.address;
+  projects.push(this);
+};
