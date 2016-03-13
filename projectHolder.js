4ddb55cd8b9cb056717e29912e8994efddfe667c
Tuesday-Last <sgnortz@gmail>
2016-03-13 14:03:55 -0700

    more CSSin'

--- projectHolder.js
+++ projectHolder.js
@@ -7,4 +7,28 @@ var rawProjects = [
     publishDate: "Feb 1st, 2016",
 
 },
+{
+    title:       'Baseball Ipsum',
+    category:    'sports',
+    image:      'Felix Hernandez',
+    address:   'http://www.baseball-reference.com/players/h/hernafe02.shtml',
+    publishDate: '2015-11-04',
+    blerb:        '<p>Baseball ipsum dolor sit amet base on balls full count leadoff. National pastime stance pine tar dribbler rookie dodgers hardball plate. <img src="http://lorempixel.com/400/200/" class="pull-left" >Club sidearm shift red sox ground rule double, left fielder pennant good eye rubber game. Steal petey center field blue wins arm visitors. Out rope base cookie grass bandbox error. </p>'
+  },
+  {
+    title:       'Cat Ipsum',
+    category:    'pets',
+    image:      'Meow Meow',
+    address:   'https://http.cat/',
+    publishDate: '2015-11-03',
+    blerb:        '<p>Find empty spot in cupboard and sleep all day refuse to leave cardboard box for make meme, make cute face for lick the plastic bag yet stare at wall turn and meow stare at wall some more meow again continue staring.<img src="http://placekitten.com/203/310" class="pull-right"> Damn that dog meow, for shake treat bag rub face on owner sleep in the bathroom sink poop in litter box, scratch the walls destroy couch as revenge.</p>'
+  },
+  {
+    title:       'Cheese Ipsum',
+    category:    'food',
+    image:      'El Rey de Queso',
+    address:   'https://www.youtube.com/watch?v=oHg5SJYRHA0',
+    publishDate: '2015-11-02',
+    blerb:        '<p>I love cheese, especially edam squirty cheese. Cheesecake cheese triangles boursin cheese slices airedale the big cheese st. agur blue cheese st. agur blue cheese. Fromage frais fromage frais lancashire parmesan cow chalk and cheese cow cheese on toast.</p>'
+  },
 ]
